eb169fc164bc1dc27878bb9867a1fff6fd8570c5
William Herring <73984595+william-herring@users.noreply.github.com>
2022-05-10 18:01:02 +1000

    created user serializer, added DRF to requirements.txt

--- server/api/urls.py
+++ server/api/urls.py
@@ -1,5 +1,7 @@
 from django.contrib import admin
+from .views import *
 from django.urls import path, include
 
 urlpatterns = [
+
 ]
--- server/server/settings.py
+++ server/server/settings.py
@@ -36,7 +36,6 @@ INSTALLED_APPS = [
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
-    'django.contrib.messages',
     'django.contrib.staticfiles',
     'api.apps.ApiConfig'
 ]
