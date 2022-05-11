e9110d60886b717de73b4a73418c2f345186f13e
William Herring <73984595+william-herring@users.noreply.github.com>
2022-05-11 17:47:02 +1000

    added rest_framework to installed apps, corrected database config

--- server/server/settings.py
+++ server/server/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/3.2/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.2/ref/settings/
 """
-
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'rest_framework',
     'api.apps.ApiConfig'
 ]
 
@@ -77,9 +78,9 @@ WSGI_APPLICATION = 'server.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'liveed',
+        'NAME': 'wavedb',
         'USER': 'williamherring',
-        'PASSWORD': 'Bigmonkey#22',
+        'PASSWORD': os.environ.get('DATABASE_PASSWORD'),
         'HOST': 'localhost',
         'PORT': '5432',
     }
