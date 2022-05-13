118ea45b17ea1c1a40def7d44e41e1493ef419d5
William Herring <73984595+william-herring@users.noreply.github.com>
2022-05-13 17:59:05 +1000

    Set up login pageview

--- client/lib/screens/login_view.dart
+++ client/lib/screens/login_view.dart
@@ -8,7 +8,7 @@ class LoginView extends StatefulWidget {
 }
 
 class _LoginViewState extends State<LoginView> {
-  late final ScrollController pageController;
+  late final PageController pageController;
 
   @override
   void initState() {
@@ -19,61 +19,152 @@ class _LoginViewState extends State<LoginView> {
   @override
   Widget build(BuildContext context) {
     return PageView(
+      controller: pageController,
       physics: const NeverScrollableScrollPhysics(),
       children: [
-        Scaffold(
-            floatingActionButton: GestureDetector(
-              onTap: () {},
-              child: Text("Create an account", style: TextStyle(color: Colors.red[400], fontWeight: FontWeight.bold)),
-            ),
-            body: Center(
-              child: Container(
-                width: 300,
-                height: 600,
-                padding: const EdgeInsets.fromLTRB(16.0, 80.0, 16.0, 0.0),
-                child: Column(
-                  children: [
-                    Image.asset('assets/images/logo.png', scale: 26),
-                    Form(
-                        child: Column(
-                          children: [
-                            TextFormField(decoration: const InputDecoration(
-                              label: Text('Email'),
-                            )),
-                            const SizedBox(height: 16),
-                            TextFormField(obscureText: true, decoration: const InputDecoration(
-                              label: Text('Password'),
-                            )),
-                            Container(
-                              margin: const EdgeInsets.only(top: 16.0),
-                              child: InkWell(
-                                onTap: () {},
-                                borderRadius: BorderRadius.circular(360),
-                                child: Ink(
-                                  padding: const EdgeInsets.fromLTRB(16.0, 12.0, 16.0, 12.0),
-                                  child: const Text(
-                                    'Sign in',
-                                    style: TextStyle(color: Colors.white, fontSize: 18),
-                                  ),
-                                  decoration: BoxDecoration(
-                                      color: Colors.red[500],
-                                      borderRadius: BorderRadius.circular(360)
-                                  ),
-                                ),
-                              ),
-                            ),
-                          ],
-                        )
-                    )
-                  ],
-                ),
-                decoration: const BoxDecoration(
-                  borderRadius: BorderRadius.all(Radius.circular(16)),
-                ),
-              ),
-            )
-        ),
+        LoginPage(onPageChange: () => pageController.nextPage(duration: const Duration(milliseconds: 300), curve: Curves.easeIn)),
+        RegisterPage(onPageChange: () => pageController.previousPage(duration: const Duration(milliseconds: 300), curve: Curves.easeIn))
       ],
     );
   }
 }
+
+class LoginPage extends StatefulWidget {
+  Function() onPageChange;
+  LoginPage({Key? key, required this.onPageChange}) : super(key: key);
+
+  @override
+  _LoginPageState createState() => _LoginPageState(onPageChange);
+}
+
+class _LoginPageState extends State<LoginPage> {
+  late Function() onPageChange;
+  _LoginPageState(this.onPageChange);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        floatingActionButton: GestureDetector(
+          onTap: onPageChange,
+          child: Text("Create an account", style: TextStyle(color: Colors.red[400], fontWeight: FontWeight.bold)),
+        ),
+        body: Center(
+          child: Container(
+            width: 300,
+            height: 600,
+            padding: const EdgeInsets.fromLTRB(16.0, 80.0, 16.0, 0.0),
+            child: Column(
+              children: [
+                Image.asset('assets/images/logo.png', scale: 26),
+                Form(
+                    child: Column(
+                      children: [
+                        TextFormField(decoration: const InputDecoration(
+                          label: Text('Email'),
+                        )),
+                        const SizedBox(height: 16),
+                        TextFormField(obscureText: true, decoration: const InputDecoration(
+                          label: Text('Password'),
+                        )),
+                        Container(
+                          margin: const EdgeInsets.only(top: 16.0),
+                          child: InkWell(
+                            onTap: () {},
+                            borderRadius: BorderRadius.circular(360),
+                            child: Ink(
+                              padding: const EdgeInsets.fromLTRB(16.0, 12.0, 16.0, 12.0),
+                              child: const Text(
+                                'Sign in',
+                                style: TextStyle(color: Colors.white, fontSize: 18),
+                              ),
+                              decoration: BoxDecoration(
+                                  color: Colors.red[500],
+                                  borderRadius: BorderRadius.circular(360)
+                              ),
+                            ),
+                          ),
+                        ),
+                      ],
+                    )
+                )
+              ],
+            ),
+            decoration: const BoxDecoration(
+              borderRadius: BorderRadius.all(Radius.circular(16)),
+            ),
+          ),
+        )
+    );
+  }
+}
+
+class RegisterPage extends StatefulWidget {
+  Function() onPageChange;
+  RegisterPage({Key? key, required this.onPageChange}) : super(key: key);
+
+  @override
+  _RegisterPageState createState() => _RegisterPageState(onPageChange);
+}
+
+class _RegisterPageState extends State<RegisterPage> {
+  late Function() onPageChange;
+  _RegisterPageState(this.onPageChange);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        floatingActionButton: GestureDetector(
+          onTap: onPageChange,
+          child: Text("Sign in", style: TextStyle(color: Colors.red[400], fontWeight: FontWeight.bold)),
+        ),
+        body: Center(
+          child: Container(
+            width: 300,
+            height: 600,
+            padding: const EdgeInsets.fromLTRB(16.0, 80.0, 16.0, 0.0),
+            child: Column(
+              children: [
+                Image.asset('assets/images/logo.png', scale: 26),
+                Form(
+                    child: Column(
+                      children: [
+                        TextFormField(decoration: const InputDecoration(
+                          label: Text('Email'),
+                        )),
+                        const SizedBox(height: 16),
+                        TextFormField(obscureText: true, decoration: const InputDecoration(
+                          label: Text('Password'),
+                        )),
+                        Container(
+                          margin: const EdgeInsets.only(top: 16.0),
+                          child: InkWell(
+                            onTap: () {},
+                            borderRadius: BorderRadius.circular(360),
+                            child: Ink(
+                              padding: const EdgeInsets.fromLTRB(16.0, 12.0, 16.0, 12.0),
+                              child: const Text(
+                                'Sign up',
+                                style: TextStyle(color: Colors.white, fontSize: 18),
+                              ),
+                              decoration: BoxDecoration(
+                                  color: Colors.red[500],
+                                  borderRadius: BorderRadius.circular(360)
+                              ),
+                            ),
+                          ),
+                        ),
+                      ],
+                    )
+                )
+              ],
+            ),
+            decoration: const BoxDecoration(
+              borderRadius: BorderRadius.all(Radius.circular(16)),
+            ),
+          ),
+        )
+    );
+  }
+}
+
+
