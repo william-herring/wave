1a40b46745a9ee587c583ff1b2cb3545b4e0ce1e
William Herring <73984595+william-herring@users.noreply.github.com>
2022-05-28 15:16:36 +1000

    Added LICENSE

--- client/lib/screens/home_screen.dart
+++ client/lib/screens/home_screen.dart
@@ -10,6 +10,20 @@ class HomeScreen extends StatelessWidget {
         title: const Text('Home'),
         leading: IconButton(icon: const Icon(Icons.menu), onPressed: () => Scaffold.of(context).openDrawer()),
       ),
+      body: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: [
+            const Text('Quick actions', style: TextStyle(fontWeight: FontWeight.w600, fontSize: 32.0)),
+            const SizedBox(height: 16.0),
+            Divider(
+              thickness: 1.2,
+              color: Colors.red[400],
+            )
+          ]
+        ),
+      )
     );
   }
 }
